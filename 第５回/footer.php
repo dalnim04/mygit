997c3fc929a17f3f04b15ff89401d47dfc84a199
dalnim04 <dalnimswang@gmail.com>
2024-11-09 13:37:34 +0900

    Add files via upload

--- 第５回/footer.php
+++ 第５回/footer.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 
-    
-    
-    <title>Document</title>
-</head>
-<body>
 
 
 <footer>
@@ -17,5 +6,3 @@
         <img src="img/logo.png" alt="Logo">
     </div>
 </footer>
-</body>
-</html>
